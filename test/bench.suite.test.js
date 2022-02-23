bbf9fd0a97cffd17baed005e1f668f71caebabaf
Dzmitry Liancheuski <dima.lenchevskii@gmail.com>
2022-02-23 06:28:19 +0300

    tiny repair

--- test/bench.suite.test.js
+++ test/bench.suite.test.js
@@ -1,14 +1,14 @@
 const R = require('ramda')
 const MORI = require('mori')
 const TEST_LIST = [3, 4, 5]
-const BS_GEN_HANDLER = require('../source/struct-master/bench-suite/bench.suite.generator.handler')
+const S_GEN_HANDLER = require('../source/struct-master/bench-suite/suite.generator.handler')
 
 test(
   `'repeatMap'     function test`,
-  () => expect(BS_GEN_HANDLER.repeatMap(3, [0, 1, 2], R.add(1))).toEqual(TEST_LIST)
+  () => expect(S_GEN_HANDLER.repeatMap(3, [0, 1, 2], R.add(1))).toEqual(TEST_LIST)
 )
 
 test(
   `'repeatMoriMap' function test`,
-  () => expect(MORI.intoArray(BS_GEN_HANDLER.repeatMoriMap(3, MORI.take(3, MORI.range()), R.add(1)))).toEqual(TEST_LIST)
+  () => expect(MORI.intoArray(S_GEN_HANDLER.repeatMoriMap(3, MORI.take(3, MORI.range()), R.add(1)))).toEqual(TEST_LIST)
 )
