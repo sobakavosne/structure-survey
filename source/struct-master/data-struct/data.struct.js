288698c34d005ebf9b6f6ad17893f7c49254d746
Dzmitry Liancheuski <dima.lenchevskii@gmail.com>
2022-03-25 10:12:27 +0400

    repair generator

--- source/struct-master/data-struct/data.struct.js
+++ source/struct-master/data-struct/data.struct.js
@@ -5,8 +5,8 @@ const ENV = H.eitherObjValuesToNumber(require('dotenv').config().parsed)
 const DS_GEN = require('./data.struct.generator')
 
 module.exports = {
-  I_LIST: DS_GEN.constructIList(ENV.STRUCT_MAX),
-  M_LIST: DS_GEN.constructMList(ENV.STRUCT_MAX),
-  N_LIST: DS_GEN.constructNList(ENV.STRUCT_MAX),
-  L_LIST: DS_GEN.constructLList(ENV.STRUCT_MAX)
+  I_LIST: DS_GEN.constructNumIList(ENV.STRUCT_MAX),
+  M_LIST: DS_GEN.constructNumMList(ENV.STRUCT_MAX),
+  N_LIST: DS_GEN.constructNumNList(ENV.STRUCT_MAX),
+  L_LIST: DS_GEN.constructNumLList(ENV.STRUCT_MAX)
 }
