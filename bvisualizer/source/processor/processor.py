59ef9ce6e14982dc9b923ceb65e47b1445c377c9
Dzmitry Liancheuski <dima.lenchevskii@gmail.com>
2022-04-18 04:43:05 +0400

    remove 'structure to function' incomplete case

--- bvisualizer/source/processor/processor.py
+++ bvisualizer/source/processor/processor.py
@@ -21,16 +21,3 @@ def run_fnc_to_struct_processorIO(log_dir, lib_number):
     P_HANDLER.construct_fnc_to_struct_case(lib_number),
     P_HANDLER.prepareBenchDataIO
   )(log_dir)
-
-
-def run_struct_to_fnc_processorIO(log_dir, lib_number):
-  """
-  -- construct `structure to function` correspondence
-  returns list of type [{"{'fnc': FNC, 'lib': LIB }": [[speed]]}]
-  """
-  return H.compose(
-    H.trace,
-    P_HANDLER.set_case_label,
-    P_HANDLER.construct_fnc_to_struct_case(lib_number),
-    P_HANDLER.prepareBenchDataIO
-  )(log_dir)
