dcede2e32581d3538df487c07e3998650d2064f6
Dzmitry Liancheuski <dima.lenchevskii@gmail.com>
2022-04-29 06:52:20 +0400

    clean comments

--- bvisualizer/source/processor/processor.py
+++ bvisualizer/source/processor/processor.py
@@ -1,11 +1,6 @@
 import processor_handler as P_HANDLER
 import utils.general_helper as H
 
-# prepared_list = P_HANDLER.prepareBenchDataIO(log_dir)
-# final_matrix = P_HANDLER.construct_fnc_to_struct_case(MORI_NUM)(prepared_list)
-# labeled_matrix = P_HANDLER.set_case_label(P_HANDLER.prepareBenchDataIO(log_dir), final_matrix)
-# return H.trace(labeled_matrix)
-
 
 def run_fnc_to_struct_processor_IO(log_dir, struct_lib_number):
   """
